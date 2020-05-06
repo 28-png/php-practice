1d8df8846feb6a062e6acf30156f8deeee255283
Matt Murphy <murphy.d.matthew28@gmail.com>
2020-05-06 15:09:45 -0500

    updated

--- includes/signup.inc.php
+++ includes/signup.inc.php
@@ -46,6 +46,10 @@ else {
         mysqli_stmt_execute($stmt);
         mysqli_stmt_store_result($stmt);
         $resultCheck = mysqli_stmt_num_rows($stmt);
+        if($resultCheck > 0) {
+            header("Location: ../signup.php?error=usertaken");
+            exit();
+        }
     }
 }
 
