34a161e9cd18447b70edc51c9542bb0585ee0a9d
Matt Murphy <murphy.d.matthew28@gmail.com>
2020-05-06 14:27:38 -0500

    updated

--- includes/signup.inc.php
+++ includes/signup.inc.php
@@ -14,6 +14,10 @@ header("Location: ../signup.php?error=emptyFields&uid=".$username."&mail=".$emai
 exit();
 }
 
+else if(!filter_var($email, FILTER_VALIDATE_EMAIL) && !filter_var(!preg_filter("/^[a-zA-Z0-9]*$/", $username))) {
+    header("Location: ../signup.php?error=invalidmailuid");
+}
+
 else if(!filter_var($email, FILTER_VALIDATE_EMAIL)) {
     header("Location: ../signup.php?error=invalidmail&uid=".$username);
     exit();
