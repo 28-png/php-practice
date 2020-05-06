26f44963b7f40da47176ccaeaf8afb39d7511fb3
Matt Murphy <murphy.d.matthew28@gmail.com>
2020-05-06 15:07:49 -0500

    updated

--- includes/signup.inc.php
+++ includes/signup.inc.php
@@ -44,7 +44,8 @@ else {
     else {
         mysqli_stmt_bind_param($stmt, "s", $username);
         mysqli_stmt_execute($stmt);
-
+        mysqli_stmt_store_result($stmt);
+        $resultCheck = mysqli_stmt_num_rows($stmt);
     }
 }
 
