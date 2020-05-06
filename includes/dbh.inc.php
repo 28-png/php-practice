0702fc9ec049a183e2ee9e37715035d40db2b2a2
Matt Murphy <murphy.d.matthew28@gmail.com>
2020-05-06 10:24:38 -0500

    updated

--- includes/dbh.inc.php
+++ includes/dbh.inc.php
@@ -4,3 +4,9 @@ $serverName = "localhost";
 $dBUsername = "root";
 $dBPassword = "";
 $dBName = "loginsystem_db";
+
+$conn = mysqli_connect($serverName, $dBUsername, $dBPassword, $dBName);
+
+if () {
+
+}
