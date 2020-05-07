013bf4657db4ba21cc630a200e9141b15ce481e8
Matt Murphy <murphy.d.matthew28@gmail.com>
2020-05-07 09:38:10 -0500

    updated

--- includes/signup.inc.php
+++ includes/signup.inc.php
@@ -62,7 +62,7 @@ else {
             else {
                 $hashpwd = password_hash($password, PASSWORD_DEFAULT);
 
-                mysqli_stmt_bind_param($stmt, "sss", $username, $email);
+                mysqli_stmt_bind_param($stmt, "sss", $username, $email, $hashpwd);
                 mysqli_stmt_execute($stmt);
                 mysqli_stmt_store_result($stmt);
 
